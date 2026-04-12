4c3baa16631db1b865813ebbf594091f53dfe3c6
MapleShadow <13624800+crazy-maple-shadow@user.noreply.gitee.com>
2026-04-12 12:21:33 +0800

    init:testmod

---
+++ slay-the-spire-2/Scripts/TestCard.cs
@@ -0,0 +1,51 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace MapleShadow.Scripts.Cards;
+
+// 加入哪个卡池
+[Pool(typeof(ColorlessCardPool))]
+public class TestCard : CustomCardModel
+{
+    // 基础耗能
+    private const int energyCost = 3;
+    // 卡牌类型
+    private const CardType type = CardType.Attack;
+    // 卡牌稀有度
+    private const CardRarity rarity = CardRarity.Rare;
+    // 目标类型（AnyEnemy表示任意敌人）
+    private const TargetType targetType = TargetType.AnyEnemy;
+    // 是否在卡牌图鉴中显示
+    private const bool shouldShowInCardLibrary = true;
+    // 卡牌的基础属性（例如这里是12点伤害）
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5, ValueProp.Move), new RepeatVar(5)]; 
+    // 卡牌的图标
+    public override string PortraitPath => "res://MapleShadow/images/cards/MapleShadow-test_card.png";
+
+    public TestCard() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    // 打出时的效果逻辑
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue) // 造成伤害，数值来源于卡牌的基础伤害属性
+            .WithHitCount(base.DynamicVars.Repeat.IntValue) 
+            .FromCard(this) // 伤害来源于这张卡牌
+            .Targeting(cardPlay.Target) // 伤害目标是玩家选择的目标
+            .Execute(choiceContext);
+    }
+
+    // 升级后的效果逻辑
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1); // 升级后增加4点伤害
+        DynamicVars.Repeat.UpgradeValueBy(1);
+    }
+}
