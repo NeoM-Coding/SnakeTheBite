28bcc403fe7334e2504ac6fbc5242bd78f95e469
MapleShadow <13624800+crazy-maple-shadow@user.noreply.gitee.com>
2026-04-13 13:02:21 +0800

    feat(MapleShadow): 添加新卡牌并更新本地化文件和版本

---
+++ slay-the-spire-2/Scripts/cards/SnakeAshCard.cs
@@ -0,0 +1,69 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace MapleShadow.Scripts.Cards;
+
+// 加入战士卡池
+[Pool(typeof(IroncladCardPool))]
+public class SnakeAshCard : MapleShadowCardModel
+{
+    // 基础耗能
+    private const int energyCost = 2;
+    // 卡牌类型
+    private const CardType type = CardType.Attack;
+    // 卡牌稀有度
+    private const CardRarity rarity = CardRarity.Common;
+    // 目标类型（AnyEnemy表示任意敌人）
+    private const TargetType targetType = TargetType.AnyEnemy;
+    // 是否在卡牌图鉴中显示
+    private const bool shouldShowInCardLibrary = true;
+
+    // 卡牌的基础属性（11点中毒）
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<PoisonPower>(11m)
+    ];
+
+    // 悬停提示
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+        new[] { HoverTipFactory.FromPower<PoisonPower>() };
+
+    public SnakeAshCard() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    // 打出时的效果逻辑
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        // 播放攻击动画
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", Owner.Character.AttackAnimDelay);
+        // 播放咬击特效
+        VfxCmd.PlayOnCreatureCenter(cardPlay.Target, "vfx/vfx_bite");
+        // 给予目标中毒，数值来源于卡牌的中毒属性
+        await PowerCmd.Apply<PoisonPower>(cardPlay.Target, DynamicVars.Poison.BaseValue, Owner.Creature, this);
+
+        // 消耗抽牌堆顶的一张牌（参考Cinder.cs写法）
+        await CardPileCmd.ShuffleIfNecessary(choiceContext, Owner);
+        CardPile drawPile = PileType.Draw.GetPile(Owner);
+        CardModel topCard = drawPile.Cards.FirstOrDefault();
+        if (topCard != null)
+        {
+            await CardCmd.Exhaust(choiceContext, topCard);
+        }
+    }
+
+    // 升级后的效果逻辑
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Poison.UpgradeValueBy(3m); // 升级后增加3层中毒（11 -> 14）
+    }
+}
---
+++ slay-the-spire-2/Scripts/potions/GoldenSnakeBloodPotion.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using System.Linq;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Merchant;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.PotionPools;
+
+namespace MapleShadow.Scripts.Potions;
+
+/// <summary>
+/// 黄金蛇血 - 罕见药水
+/// 饮用后获得25金币，可扔给队友，战斗内外皆可使用
+/// 不会出现在商店中
+/// </summary>
+[Pool(typeof(SharedPotionPool))]
+public class GoldenSnakeBloodPotion : CustomPotionModel
+{
+    // 稀有度 - 罕见（蓝）
+    public override PotionRarity Rarity => PotionRarity.Uncommon;
+
+    // 使用方式 - 任何时候都能使用（战斗内外皆可）
+    public override PotionUsage Usage => PotionUsage.AnyTime;
+
+    // 目标类型 - 任意玩家（可以扔给队友）
+    public override TargetType TargetType => TargetType.AnyPlayer;
+
+    // 动态变量 - 获得25金币
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new GoldVar(25)];
+
+    // 药水图片路径
+    // public override string? PackedImagePath => "res://MapleShadow/images/potions/MapleShadow-golden_snake_blood_potion.png";
+    // public override string? PackedOutlinePath => "res://MapleShadow/images/potions/MapleShadow-golden_snake_blood_potion.png";
+
+    // 标记是否正在商店生成过程中
+    public static bool IsInShopGeneration { get; set; }
+
+    // 使用时的效果逻辑
+    protected override async Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
+    {
+        PotionModel.AssertValidForTargetedPotion(target);
+        await PlayerCmd.GainGold(DynamicVars.Gold.BaseValue, target.Player!);
+    }
+}
+
+/// <summary>
+/// 商店库存生成补丁：在商店生成药水时将黄金蛇血加入黑名单
+/// </summary>
+[HarmonyPatch(typeof(MerchantInventory), "PopulatePotionEntries")]
+public static class MerchantInventoryPotionPatch
+{
+    static void Prefix()
+    {
+        GoldenSnakeBloodPotion.IsInShopGeneration = true;
+    }
+
+    static void Finalizer()
+    {
+        GoldenSnakeBloodPotion.IsInShopGeneration = false;
+    }
+}
+
+/// <summary>
+/// 商店补货补丁：在商店补货时将黄金蛇血加入黑名单
+/// </summary>
+[HarmonyPatch(typeof(MerchantPotionEntry), "FillSlot")]
+public static class MerchantPotionEntryRestockPatch
+{
+    static void Prefix()
+    {
+        GoldenSnakeBloodPotion.IsInShopGeneration = true;
+    }
+
+    static void Finalizer()
+    {
+        GoldenSnakeBloodPotion.IsInShopGeneration = false;
+    }
+}
+
+/// <summary>
+/// 药水选项过滤器：当处于商店生成流程时，排除黄金蛇血
+/// </summary>
+[HarmonyPatch(typeof(PotionFactory), nameof(PotionFactory.GetPotionOptions))]
+public static class PotionFactoryShopExclusionPatch
+{
+    static void Postfix(ref IEnumerable<PotionModel> __result)
+    {
+        if (GoldenSnakeBloodPotion.IsInShopGeneration)
+        {
+            __result = __result.Where(p => p is not GoldenSnakeBloodPotion);
+        }
+    }
+}
