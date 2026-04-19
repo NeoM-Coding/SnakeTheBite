3090fe749333765e53c6d3180a0481e1b4fdab11
MapleShadow <13624800+crazy-maple-shadow@user.noreply.gitee.com>
2026-04-19 14:49:31 +0800

    feat(MapleShadow): 添加新卡牌灼热蛇咬

---
+++ slay-the-spire-2/Scripts/cards/SearingSnakebiteCard.cs
@@ -0,0 +1,49 @@
+// 灼热蛇咬 - 2费红卡攻击，保留，给予敌人7层中毒，可无限升级
+using System;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace MapleShadow.Scripts.Cards;
+
+[Pool(typeof(IroncladCardPool))]
+public class SearingSnakebiteCard : MapleShadowCardModel
+{
+    private const int energyCost = 2;
+    private const CardType type = CardType.Attack;
+    private const CardRarity rarity = CardRarity.Common;
+    private const TargetType targetType = TargetType.AnyEnemy;
+    private const bool shouldShowInCardLibrary = true;
+
+    // 可无限升级
+    public override int MaxUpgradeLevel => int.MaxValue;
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PoisonPower>(7m)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CardKeyword.Retain), HoverTipFactory.FromPower<PoisonPower>()];
+
+    public SearingSnakebiteCard() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", Owner.Character.AttackAnimDelay);
+        await PowerCmd.Apply<PoisonPower>(cardPlay.Target, DynamicVars["PoisonPower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        // 升级增量逐级递增：+3, +4, +5, +6, ...
+        DynamicVars["PoisonPower"].BaseValue += 3m + (CurrentUpgradeLevel - 1);
+    }
+}
